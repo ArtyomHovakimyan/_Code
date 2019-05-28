9ee8bcf41baaaa7b45a6ca0d957ba2a8bf4049d5
User <artyom.hovakimyan01@gmail.com>
2019-05-29 03:21:09 +0400

    part6

--- Mic.Volo.CodeRepeat/Mic.Volo.CodeRepeat/Program.cs
+++ Mic.Volo.CodeRepeat/Mic.Volo.CodeRepeat/Program.cs
@@ -4,57 +4,13 @@ using System.Collections.ObjectModel;
 
 namespace Mic.Volo.CodeRepeat
 {
-    public class class1
-    {
-        public virtual string DoSomething()
-        {
-            return "class1";
-        }
-    }
-
-    public class class2 : class1
-    {
-        public override string DoSomething()
-        {
-            return "class2";
-        }
-    }
-
-    public class class3 : class2
-    {
-        public new string DoSomething()
-        {
-            return "Class3";
-        }
-    }
-
-    class Base
-    {
-        public virtual string GetName() => "BaseName";
-    }
-    class Child:Base
-    {
-        public override string GetName()
-        {
-            return "ChildName";
-        }
-    }
-    class Other:Child
-    {
-        public override string GetName()
-        {
-            return "Other Name";
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            //class1 a = new class3();
-            //Console.WriteLine(a.DoSomething());
-
-            Base @base = new Other();
-            Console.WriteLine(@base.GetName());
+            int? i = null;
+            i++;
+            Console.WriteLine("Value of i is {0} ", i);
         }
     }
 }
