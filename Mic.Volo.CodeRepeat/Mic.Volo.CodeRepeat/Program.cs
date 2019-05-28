9ec674a34a5c9ab0180c8d250cc7bf5a971e2f50
User <artyom.hovakimyan01@gmail.com>
2019-05-29 02:40:17 +0400

    dynamic

---
+++ Mic.Volo.CodeRepeat/Mic.Volo.CodeRepeat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mic.Volo.CodeRepeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 100;
+            dynamic d = b;
+            Console.WriteLine(d.GetType());
+            d += 100;
+            Console.WriteLine(d.GetType());
+        }
+    }
+}
