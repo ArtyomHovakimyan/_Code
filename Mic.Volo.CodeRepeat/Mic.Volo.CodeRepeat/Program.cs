eca39545940a46a568d277df1441e37ec855c7e9
User <artyom.hovakimyan01@gmail.com>
2019-05-30 02:12:29 +0400

    part10

--- Mic.Volo.CodeRepeat/Mic.Volo.CodeRepeat/Program.cs
+++ Mic.Volo.CodeRepeat/Mic.Volo.CodeRepeat/Program.cs
@@ -5,37 +5,54 @@ using System.Threading;
 
 namespace Mic.Volo.CodeRepeat
 {
+    class GameScore
+    {
+        string user;
+        int age;
+        public GameScore()
+        {
+            user = "Smith";
+            age = 28;
+            Console.WriteLine("Previous User {0} and he was {1} year old",user,age);
+        }
+        public GameScore(string name,int age1)
+        {
+            user = name;
+            age = age1;
+            Console.WriteLine("Current User {0} and he is {1} year old",user,age);
+        }
+    }
     class Program
     {
-        static int counter = 0;
-        static object locker = new object();
+       
+        static IEnumerable<int> YieldReturn()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+        static string _name;
+        static string Name
+        {
+            get
+            {
+                return _name ?? "Default";
+            }
+            set
+            {
+                _name = value;
+            }
+        }
         static void Main(string[] args)
         {
-
-            new Thread(PrintStar).Start();
-            new Thread(PrintHashTag).Start();
+            Double i = Double.NaN;
+            while (i == i)
+            {
+                Console.WriteLine("Hello");
+            }
 
             Console.ReadLine();
         }
-        static void PrintStar()
-        {
-            lock(locker)
-            {
-                for (counter = 0; counter < 5; counter++)
-                {
-                    Console.WriteLine("*"+"\t");
-                }
-            }
-        }
-        public static void PrintHashTag()
-        {
-            lock (locker)
-            {
-                for (counter = 0; counter < 5; counter++)
-                {
-                    Console.WriteLine("#" + "\t");
-                }
-            }
-        }
+        
     }
 }
